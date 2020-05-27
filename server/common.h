b8245b4be92682e84d5a9f857f745342a6f8957b
Catnip0709 <13347629779@163.com>
2020-05-27 14:53:32 +0800

    管理员修改物资库存：新增对物资不足的处理

--- server/common.h
+++ server/common.h
@@ -18,7 +18,7 @@
 #define NOT_INCHARGE        1004 // 当前管理员对该栋楼无管理权限
 #define ILLEGAL_REQUEST     1005 // 非法请求
 #define NOT_ADMINISTRATOR   1006 // 该用户不是管理员
-#define DATA_NULL           1007 // 数据缺失
+#define NOT_ENOUGH          1007 // 物资不足
 
 #include<iostream>
 #include<string>
